1527daa4517cf808d74ee1701857a135d0c8c2d3
charliekroon <charlotkroon@protonmail.com>
2021-09-20 13:12:28 +0200

    create full_name

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -2,8 +2,9 @@ class Types::AuthorType <Types::BaseObject
   description "An author"
 
   field :id, ID, null: false
-  field :first_name, String, null: true
-  field :last_name, String, null: true
+  field :first_name, String, null: true, camelize: false
+  field :last_name, String, null: true, camelize: false
   field :yob, Int, null: false
-  field :is_alive, Boolean, null: true
+  field :is_alive, Boolean, null: true, camelize: false
+  field :full_name, String, null: true, camelize: false
 end
